2c54d92ff37eef5bf9d6a090805171b07ace0d6b
lohit <lohithr60@gmail.com>
2024-03-09 23:00:36 +0530

    Commit the changes

---
+++ MovieRecommendationSystem/app/urls.py
@@ -0,0 +1,11 @@
+# urls.py
+
+from django.urls import path
+from .views import *
+
+urlpatterns = [
+    path('/',home,name='home'),
+    path('', navbar, name='navbar'),
+    path('login', login, name='login'),
+    path('register', register_view, name='register'),
+]
